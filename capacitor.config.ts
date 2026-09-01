b192df4dd5e6c653fbf18ea18e9bce4a64076376
Sun <yyxxit@163.com>
2026-08-31 21:11:31 -0700

    fix: 修改安卓应用名称

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'com.bugc.admin',
-  appName: 'BugC Admin',
+  appName: 'BugC',
   webDir: 'dist-android',
   server: {
     androidScheme: 'https'
